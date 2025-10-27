631cce28a01465517521500fadbf6b1e8d74b2f6
Lakshan-Madushanka <epmadushanka@gmail.com>
2025-10-27 18:39:19 +0530

    feat: optimize

--- app/Http/Controllers/Api/Setup/CheckEnvController.php
+++ app/Http/Controllers/Api/Setup/CheckEnvController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Setup;
 use App\Actions\Setup\CheckEnvAction;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Cache;
 
 class CheckEnvController extends Controller
 {
@@ -15,6 +16,8 @@ class CheckEnvController extends Controller
 
     public function generateKey(CheckEnvAction $checkENV): JsonResponse
     {
+        Cache::put('setup_status', 'completed');
+
         return response()->json(['status' => $checkENV->generateAppKey()]);
     }
 }
--- app/Http/Controllers/Api/Setup/OptimizeController.php
+++ app/Http/Controllers/Api/Setup/OptimizeController.php
@@ -5,15 +5,12 @@ namespace App\Http\Controllers\Api\Setup;
 use App\Actions\Setup\OptimizeAction;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Cache;
 
 class OptimizeController extends Controller
 {
 
     public function __invoke(OptimizeAction $action): JsonResponse
     {
-        Cache::put('setup_status', 'completed');
-
         $status = $action->execute();
 
         return response()->json(['status' => $status]);
