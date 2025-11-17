69917b5e5036e8956bdbe1132487d0cf26340016
Lakshan-Madushanka <epmadushanka@gmail.com>
2025-11-17 12:59:36 +0530

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,5 @@
 <?php
 
-use App\Enums\Role;
-use App\Models\User;
-use Database\Data\GNData;
-use Database\Data\IQData;
-use Database\Data\IQPatternsData;
-use Database\Data\ProgrammingData;
-use Database\Data\UserData;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,30 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/test', function () {
-    $status = \Illuminate\Support\Facades\Artisan::call('optimize:clear');
-
-    Illuminate\Support\Facades\Artisan::call('migrate:fresh', ['--force' => true]);
-    UserData::seedUsers();
-    ProgrammingData::seedQuestions();
-    IQData::seedQuestions();
-    GNData::seedQuestions();
-    IQPatternsData::seedQuestions();
-
-    $superAdminEmail = 'super-admin@company.com';
-
-    User::whereEmail($superAdminEmail)->existsOr(function () {
-        User::create([
-            'name' => 'Super Admin',
-            'role' => Role::SUPER_ADMIN,
-            'password' => Hash::make('superAdmin123'),
-            'email' => 'super-admin@company.com',
-        ]);
-    });
-    return 'ok....status ' . $status;
-});
-
 Route::get('/', function () {
     return view('index');
 });
